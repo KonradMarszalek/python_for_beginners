fb7c2a56b93f9c609c7b8f2d00ad5670ae63ddb0
Konrad Marszalek <kmarszalek@atlassian.com>
2019-03-18 14:04:29 +0100

    Fix paintings app

--- paintings/polls/views.py
+++ paintings/polls/views.py
@@ -1,5 +1,26 @@
-from django.http import HttpResponse
+import os
+from pathlib import Path
+import random
+
+from django.conf import settings
+from django.shortcuts import render
 
 
 def hello(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    images = []
+    relative_static_dir = os.path.join(settings.STATIC_URL.strip("/"), "polls")
+    absolute_static_dir = Path(settings.BASE_DIR).joinpath(relative_static_dir)
+    pattern = "*.jpg"
+
+    for image in absolute_static_dir.glob(pattern):
+        images.append(image.name)
+
+    random_image = random.choice(images)
+
+    return render(request, 'polls/index.html', context={'random_image': os.path.join("polls", random_image)})
+
+
+
+
+
+
